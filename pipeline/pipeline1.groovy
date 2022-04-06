d040c9b51fa7aa1c5527ac14b42f6249f60a5e9d
ankit dhakate <92425685+ankitdhakate@users.noreply.github.com>
2022-04-06 17:00:42 +0530

    Update pipeline1.groovy

--- pipeline/pipeline1.groovy
+++ pipeline/pipeline1.groovy
@@ -8,7 +8,7 @@ pipeline {
         }
         stage('build') {
             steps {
-                echo 'build successful' 
+                sh 'mnv clean package'
             }
         }
         stage('test') {
